78c90fa6f5144c5e44955adc1e28495d8f99eccb
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2020-12-02 22:03:26 +0200

    Created docker build Image func

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ let aws = {
     'secret_access_key': core.getInput('secret_access_key'),
     'region': core.getInput('region'),
     'aws_account_id': core.getInput('aws_account_id'),
+    'repo_name': core.getInput('repo_name'),
+    'scan_on_push': core.getInput('scan_on_push'),
+    'image_name': core.getInput('image_name'),
+    'use_compose': core.getInput('use_compose'),
+    'docker_path': core.getInput('docker_path'),
     'force_push': core.getInput('force_push'),
     'working_dir': core.getInput('working-directory')
 }
@@ -48,3 +53,42 @@ const login = ({ region, aws_account_id }) => {
     }
 }
 
+const createRepository = ({ repo_name, region, scan_on_push }) => {
+    try {
+        const repo = execSync(`
+        aws ecr create-repository \
+        --repository-name ${repo_name} \
+        --image-scanning-configuration scanOnPush=${scan_on_push} \
+        --region ${region}
+        `).toString();
+        console.log(repo);
+    } catch (error) {
+        console.log(error.message);
+        core.setFailed(error.message);
+    }
+}
+
+const buildImage = ({ use_compose, image_name, docker_path }) => {
+    try {
+        if (use_compose === 'true') {
+            const build = execSync(`docker-compose build`).toString();
+            console.log(build);
+
+        } else {
+            const build = execSync(`docker build -t ${image_name} ${docker_path}`).toString();
+            console.log(build);
+        }
+    } catch (error) {
+        console.log(error.message);
+        core.setFailed(error.message);
+    }
+}
+
+const tagImage = ({ }) => {
+    try {
+
+    } catch (error) {
+        console.log(error.message);
+        core.setFailed(error.message);
+    }
+}
