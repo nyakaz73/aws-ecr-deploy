dd16f2b5db039f3aaf1d418be31711fe9182a90f
nyakaz73 <tafadzwalnyamukapa@gmail.com>
2020-12-02 19:54:51 +0200

    Added login fun and createAWS for login func

---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const { execSync } = require('child_process');
+
+//Variables
+let aws = {
+    'access_key_id': core.getInput('access_key_id'),
+    'secret_access_key': core.getInput('secret_access_key'),
+    'region': core.getInput('region'),
+    'aws_account_id': core.getInput('aws_account_id'),
+    'force_push': core.getInput('force_push'),
+    'working_dir': core.getInput('working-directory')
+}
+
+//Exec options
+let execOptions = {
+    cwd: aws.working_dir,
+}
+
+//Create a .aws folder in the root home directory to store security config and credentials files.
+const createAwsForLogin = ({ access_key_id, secret_access_key, region }) => {
+    execSync(`cat >~/.aws/config <<EOF
+[default]
+region = ${region}
+output = json
+    `);
+    execSync(`cat >~/.aws/credentials <<EOF
+[default]
+aws_access_key_id = ${access_key_id}
+aws_secret_access_key = ${secret_access_key}
+    `);
+}
+
+
+const login = ({ region, aws_account_id }) => {
+    try {
+        createAwsForLogin(aws);
+        const login = execSync(`aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com`).toString();
+        console.log(login);
+    } catch (error) {
+        console.log(error.message);
+        core.setFailed(error.message);
+    }
+}
+
